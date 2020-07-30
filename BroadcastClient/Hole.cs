1290f31f37d2cf9c1bb92382a7716e2ba71932a4
Rackover <rackover@racknet.noip.me>
2020-07-30 22:52:48 +0200

    Enhanced hole punching

--- BroadcastClient/Hole.cs
+++ BroadcastClient/Hole.cs
@@ -11,23 +11,34 @@ namespace Broadcast.Client
 {
     public static class Hole
     {
+        const byte HOLES_COUNT = 10;
+
         public async static Task PunchUDP(string ipAddress, ushort port, Logger logger)
         {
             while (true)
             {
                 try
                 {
-                    using (UdpClient c = new UdpClient(port))
+                    using (Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
                     {
-                        c.Send(Enumerable.Range(0, 255).Select(o => (byte)Networking.PROTOCOL_GARBAGE_PUNCH).ToArray(), 255, ipAddress, port);
+                        sock.Connect(ipAddress, port);
+                        for (int i = 0; i < HOLES_COUNT; i++)
+                        {
+                            logger.Trace("Punching " + ipAddress + ":" + port+" ("+i+"/"+HOLES_COUNT+")");
+
+                            // Create a 255-filled packet
+                            sock.Send(Enumerable.Range(0, 255).Select(o => (byte)Networking.PROTOCOL_GARBAGE_PUNCH).ToArray());
+
+                            await Task.Delay(100);
+                        }
                         logger.Info("Finished punching " + ipAddress + ":" + port);
                         break;
                     }
                 }
-                catch (SocketException)
+                catch (SocketException e)
                 {
                     // Do nothing, wait for next opportunity
-                    logger.Trace("Could not punch {0}:{1}, waiting for next opportunity".Format(ipAddress, port));
+                    logger.Trace("Could not punch {0}:{1}, waiting for next opportunity ({2})".Format(ipAddress, port, e.ToString()));
                     await Task.Delay(100);
                 }
             }
